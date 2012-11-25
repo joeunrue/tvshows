92279ce8e4bb4c5cd1df56d783f5cdf22bb7543d
Saxon Unrue <joe@unrue.com>
2012-11-25 15:40:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tvshows::Application.config.secret_token = '5bf76635d72796327f8463930ea7a3e6c7d6b496235e8047ea556fd0863e5d088e33d755a1aeed215b409cad4a66f59c22168e30c33def901d2f673ea702238b'
