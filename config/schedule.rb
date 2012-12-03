7835c51c81381adfbc9c1bc34744254d664f84a3
Saxon Unrue <joe@unrue.com>
2012-12-03 14:48:41 -0500

    Made a huge mess of the schedule.rb file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,26 +1,13 @@
+env :PATH, ENV['PATH']
 set :output, "#{path}/log/cron.log"
-job_type :script, "'#{path}/script/:task' :output"
+set :job_template, "/bin/zsh -l -c -i ':job'"
 
-every 15.minutes do
-  runner 'Feed.parse_all', :environment => :development
+job_type :runner,
+  "cd :path && bin/rails runner -e :environment ':task' :output"
+job_type :rake,
+  "cd :path && RAILS_ENV=:environment bin/rake :task :output"
+
+every 1.hour do
+  rake 'feeds:parse', :environment => :development
+  #runner 'Feed.parse_all', :environment => :development
 end
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
--- lib/tasks/parse_feeds.rake
+++ lib/tasks/parse_feeds.rake
@@ -2,5 +2,6 @@ namespace :feeds do
   desc 'Checks feeds for new files'
   task :parse => :environment do
     Feed.parse_all
+    puts "Feed has been parsed - #{Time.now}"
   end
 end
