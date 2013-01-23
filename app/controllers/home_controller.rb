c41339d2d933ac113f41609c2b2acd0fdd784c64
Saxon Unrue <joe@unrue.com>
2013-01-22 22:59:20 -0500

    Displays file formats in an organized way

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
     @recent_episodes = Episode.recent(25)
+    @file_formats = @recent_episodes.map{
+      |i| i.torrents.map{ |i| i.file_format }
+    }.flatten.uniq
   end
 end
